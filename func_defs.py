a6749ce874ec72f6058173781613ca2a425a35a7
hkristof03 <hkristof03@hotmail.com>
2018-10-01 00:35:44 +0200

    Corrected func_defs, hdf5, graph pics

--- func_defs.py
+++ func_defs.py
@@ -23,10 +23,16 @@ def dates_to_milisec(start,end):
     else:
         date_start = datetime.datetime.strptime(str(start),'%Y%m%d')
         date_end = datetime.datetime.strptime(str(end), '%Y%m%d')
+
+        #adding one day to date_start and date_end because blockhain.info has an offset....
+        date_start = date_start + datetime.timedelta(days=1)
+        date_end = date_end + datetime.timedelta(days=1)
+
         days = (date_end-date_start).days
 
     date_list = [date_start + datetime.timedelta(days=x) for x in range(0,days+1)]
-    date_list_in_ms = [date_list[x].timestamp() * 1000 for x in range(len(date_list))]
+    #date_list_in_ms = [date_list[x].timestamp() * 1000 for x in range(len(date_list))]
+    date_list_in_ms = [time.mktime(date_list[x].timetuple()) * 1000.0 for x in range(len(date_list))]
 
     return date_list_in_ms
 
@@ -62,7 +68,8 @@ def parse_block_data(block_hash):
 
     given_block_data = {}
     block_height = int(data["height"])
-    creation_time = str(datetime.datetime.fromtimestamp(float(data["time"])))   # -1 hour for my time zone
+    creation_time = str(datetime.datetime.utcfromtimestamp(float(data["time"])))   # -1 hour for my time zone
+    block_hash = data['hash']
     #other metadata to store...
 
     for i in range(0,len(data["tx"])):
@@ -89,7 +96,7 @@ def parse_block_data(block_hash):
 
         given_block_data[tr_hash] = [(inputs,inputs_values),(outputs,outputs_values)]
 
-    return given_block_data,block_height,creation_time
+    return given_block_data,block_height,creation_time, block_hash
 
 
 
@@ -115,10 +122,10 @@ def create_tr_graph(given_block_data):
 #create hdf5 file from blocks transaction matrices (adjacency matrices) with corresponding meta data and a transaction graph picture for each block
 #block heights identify the given block's transaction matrix and the corresponding graph picture. Transactions graph can not be recovered as networkx
 #multidigraph from the adjacency matrices!!! For analysing each block's directed transaction graphs the create_tr_graph function should be used!
-def append_to_hdf5_file(matrix,block_height,block_creation_time,graph_picture):
+def append_to_hdf5_file(matrix,block_height,block_creation_time,block_hash, hdf5_filename):
 
 
-    hdf5_file = h5py.File('BTC_dataset.hdf5', mode="a")
+    hdf5_file = h5py.File(hdf5_filename, mode="a")
 
     if 'transaction_matrices' not in list(hdf5_file.keys()):
         grp = hdf5_file.create_group("transaction_matrices")
@@ -134,9 +141,9 @@ def append_to_hdf5_file(matrix,block_height,block_creation_time,graph_picture):
 
         dataset.attrs['creation_time'] = block_creation_time
         dataset.attrs['block_height'] = block_height
+        dataset.attrs['block_hash'] = block_hash
         #other metadata for each block's metadata...
-        image = mpimg.imread(os.getcwd() + "\\" + str(block_height) + ".png")
-        dataset = grp.create_dataset(str(block_height) + "_picture", data=image)
+    
 
 
     hdf5_file.close()
@@ -149,15 +156,16 @@ def create_tr_graph_and_visualize(start_date,end_date):
     date_list_in_milisec = dates_to_milisec(start_date,end_date)
     block_hashes = query_block_hashes(date_list_in_milisec)
 
-    plt.figure(figsize=(50,50))
+    #plt.figure(figsize=(50,50))
 
-    block_heights = []
-    creation_times = []
+    block_heights_ = []
+    creation_times_ = []
+    block_hashes_ = []
 
-
-    #path = r'D:\bme\Szakdolgozat'
-    path = '/data/'
+    path = r'D:/bme/Szakdolgozat/Test/'
+    #path = '/data/'
     existing_blocks = glob.glob(path+"*.png")
+    hdf5_filename = path + str(start_date) + '_' + str(end_date) + '.hdf5'
 
     for n in range(len(block_hashes)):
         one_day_block_hashes = block_hashes[n]
@@ -166,7 +174,8 @@ def create_tr_graph_and_visualize(start_date,end_date):
 
             try:
 
-                given_block_data, block_height, block_creation_time = parse_block_data(one_day_block_hashes[i])
+                plt.figure(figsize=(50,50))
+                given_block_data, block_height, block_creation_time, block_hash = parse_block_data(one_day_block_hashes[i])
 
                 block_picture = str(block_height) + ".png"
 
@@ -181,11 +190,17 @@ def create_tr_graph_and_visualize(start_date,end_date):
                 nx.draw(tr_graph,node_color='r',font_size='30',node_size=50,edge_color='black',arrowsize=30)
                 plt.savefig(path + fname)
                 #graph_picture = mpimg.imread(os.getcwd() + "\\" + str(block_height) + ".png")
-                #append_to_hdf5_file(matrix,block_height,block_creation_time,graph_picture)
-                plt.clf()
+                append_to_hdf5_file(matrix,block_height,block_creation_time,block_hash, hdf5_filename)
+                #plt.clf()
 
-                block_heights.append(block_height)
-                creation_times.append(block_creation_time)
+                block_hashes_.append(block_hash)
+                block_heights_.append(block_height)
+                creation_times_.append(block_creation_time)
+
+                plt.clf()
+                plt.cla()
+                plt.close()
+                #plt.gcf().clear()
 
             except ValueError:
 
@@ -195,13 +210,8 @@ def create_tr_graph_and_visualize(start_date,end_date):
 
                 print('KeyError at day:', n, 'block hash : ', i)
 
-            except Exception as exception:
-
-                print('Output unexpected exceptions at day:', n, 'block hash:', i)
 
 
-    plt.close()
-
-    df = pd.DataFrame({'block_heights':block_heights, 'block_creation_times': creation_times})
+    df = pd.DataFrame({'block_heights':block_heights_, 'block_creation_times': creation_times_, 'block_hashes': block_hashes_})
     df_name = str(start_date) + '_' + str(end_date) + '.csv'
     df.to_csv(path + df_name, index=False)
